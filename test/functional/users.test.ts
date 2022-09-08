715ec111db3f2cfdb21dc3febed4e25ace8de03b
bertolucci-alan <bertoluccialan23@gmail.com>
2022-09-08 00:03:02 -0300

    feat: add create users test

---
+++ test/functional/users.test.ts
@@ -0,0 +1,20 @@
+jest.useFakeTimers();
+describe('User functional test', () => {
+  describe('When create a new user', () => {
+    it('should return successfully when create a new user', async () => {
+      const newUser = {
+        name: 'Alan',
+        email: 'alan@gmail.com',
+        password: '123',
+        balance: 0,
+        isAdmin: false,
+      };
+      const response = await global.testRequest
+        .post('/api/users')
+        .send(newUser);
+      console.log(response);
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual(expect.objectContaining(newUser));
+    });
+  });
+});

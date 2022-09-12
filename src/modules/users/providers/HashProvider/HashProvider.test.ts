220d8accf112a79643e72aef4963c8c843dc6b7d
bertolucci-alan <bertoluccialan23@gmail.com>
2022-09-12 00:29:39 -0300

    feat: JWTProvider unit test

--- src/modules/users/providers/HashProvider/HashProvider.test.ts
+++ src/modules/users/providers/HashProvider/HashProvider.test.ts
@@ -6,19 +6,16 @@ const hashProviderMock = mock<HashProvider>();
 describe('HashProvider unit test', () => {
   it('should return a hashed password', async () => {
     hashProviderMock.hashPassword.mockResolvedValue('fake-hash');
-    const hash = await new HashProvider().hashPassword('fake-password', 8);
-    expect(hash).toEqual(expect.any(String));
+    const hash = await hashProviderMock.hashPassword('fake-password', 8);
+    expect(hash).toEqual('fake-hash');
   });
 
   it('should return true compare password', async () => {
-    const hashFakePassword =
-      '$2b$08$6hcrDwFbPer/3CEpk42M3ecTBuqcUcyKQ.2siRVUoE3kGKkXMWpJO';
-
     hashProviderMock.comparePassword.mockResolvedValue(true);
 
-    const compareHash = await new HashProvider().comparePassword(
+    const compareHash = await hashProviderMock.comparePassword(
       'fake-password',
-      hashFakePassword
+      'fake-password-hashed'
     );
     expect(compareHash).toBeTruthy();
   });

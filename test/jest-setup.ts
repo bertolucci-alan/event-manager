1e1e1769e045655cc3b0f59200bf868cc385c08f
bertolucci-alan <bertoluccialan23@gmail.com>
2022-09-12 19:06:03 -0300

    feat: mock cache

--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -3,10 +3,15 @@ import '@src/shared/container';
 import supertest from 'supertest';
 import { SetupServer } from '@src/app';
 import { dataSource } from '@src/database';
+import { mock } from 'jest-mock-extended';
+import { CacheService } from './util/cache/CacheService';
+const cacheServiceMock = mock<CacheService>();
 
 beforeAll(async () => {
   const server = new SetupServer();
   await dataSource.initialize();
+  cacheServiceMock.getCache.mockResolvedValue(null);
+  cacheServiceMock.setCache.mockResolvedValue(null);
   global.testRequest = supertest(server.getApp());
 });
 
